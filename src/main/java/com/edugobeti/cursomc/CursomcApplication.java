4f6d15a43cbdf4f18df060790968513c14a69410
Edugobeti <edugobeti@gmail.com>
2022-10-04 11:57:18 -0300

    Operação de Instanciação automatica

--- src/main/java/com/edugobeti/cursomc/CursomcApplication.java
+++ src/main/java/com/edugobeti/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.edugobeti.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.edugobeti.cursomc.domain.Category;
+import com.edugobeti.cursomc.repository.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informatica");
+		Category cat2 = new Category(null, "Escritorio");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
