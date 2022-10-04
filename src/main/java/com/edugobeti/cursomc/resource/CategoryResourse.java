7ca911ee31d428f0de0ceb45914ae396cef1913f
Edugobeti <edugobeti@gmail.com>
2022-10-04 09:25:38 -0300

    Testando a primeira classe de dominio - Category

--- src/main/java/com/edugobeti/cursomc/resource/CategoryResourse.java
+++ src/main/java/com/edugobeti/cursomc/resource/CategoryResourse.java
@@ -1,15 +1,28 @@
 	package com.edugobeti.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.edugobeti.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResourse {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "O rest esta funcionando";
+	public List<Category> listar(){
+		
+		Category cat1 = new Category(1 , "Informatica");
+		Category cat2 = new Category(1 , "Escritorio");
+		
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
