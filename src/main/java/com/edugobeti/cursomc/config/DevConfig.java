662e5e46b7ce9cf7131f0e7b6693d6d429961dac
edugobeti <edugobeti@gmail.com>
2022-11-01 15:36:44 -0300

    Criando Profile Dev

--- src/main/java/com/edugobeti/cursomc/config/DevConfig.java
+++ src/main/java/com/edugobeti/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.edugobeti.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.edugobeti.cursomc.service.DataBaseService;
 
 @Configuration
-@Profile("test")
-public class DataBaseConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	DataBaseService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		dbService.instantiateDataBase();
 		return true;
 	}
